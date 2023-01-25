80d6d3ca27490a4c196ceb38ba95c5bae4047cb9
VictorMendesON <122651100+VictorMendesON@users.noreply.github.com>
2023-01-25 19:51:05 -0300

    Urna1.0

---
+++ Urna/README.md
@@ -0,0 +1,2 @@
+# UrnaEletronica
+Urna Eletronica feita com HTML, CSS e JS
---
+++ Urna/etapas.js
@@ -0,0 +1,58 @@
+let etapas = [
+    {
+        titulo: 'Presidente',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '13',
+                nome: 'Lula',
+                partido: 'PT',
+                vice: 'Cic',
+                fotos: [
+                    {url: 'Lula.jpg', legenda: 'Presidente'},
+                    {url: 'Geraldo Alckmin.webp', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '22',
+                nome: 'Bolsonaro',
+                partido: 'PL',
+                vice: 'Cic',
+                fotos: [
+                    {url: 'Bolsonaro.jpg', legenda: 'Presidente'},
+                    {url: 'Braga Netto.jpeg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            
+        ]
+    },
+
+    {
+        titulo: 'Governador',
+        numeros: 3,
+        candidatos: [
+            {
+                numero: '255',
+                nome: 'Eduardo Paes',
+                partido: 'PSD',
+                fotos: [
+                    {url: 'Eduardo Paes.jpg', legenda: 'Governador'},
+
+                ]
+            },
+            {
+                numero: '223',
+                nome: 'Claudio castro',
+                partido: 'PL',
+                fotos: [
+                    {url: 'Claudio castro.jpg', legenda: 'Governador'},
+
+                ]
+            },
+            
+        ]
+    }
+    
+
+
+]
